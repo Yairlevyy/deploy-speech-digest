f45fe866a67b95bca596467e8a457a5b0aa609dd
Yair Levy <yairlevyy5@gmail.com>
2023-11-27 15:23:28 +0100

    second

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -37,13 +37,13 @@ api_router.post('/upload/:userId', authenticateJWT, upload.single('audioFile'), 
     const summary = await summarize(transcript);
 
     // Delete the file after successful processing
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error('Error deleting file:', err);
-      } else {
-        console.log('File deleted successfully');
-      }
-    });
+    // fs.unlink(filePath, (err) => {
+    //   if (err) {
+    //     console.error('Error deleting file:', err);
+    //   } else {
+    //     console.log('File deleted successfully');
+    //   }
+    // });
 
     const title = req.query.title;
 
